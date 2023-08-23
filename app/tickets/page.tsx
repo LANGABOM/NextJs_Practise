f0163920b882612e1c90e66f5065ebe63d7ee854
LANGABOM <batilelanga@hotmail.com>
2023-08-23 20:37:50 +0200

    feature: init

---
+++ app/tickets/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const tickets: React.FC<any> = () => {
+    return (
+        <>
+        <div>
+            data here
+        </div>
+        </>
+    )
+}
+
+export default tickets;
